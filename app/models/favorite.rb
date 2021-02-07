22cc53401ccdd0898c42cba465781ac030fa35d8
EC2 Default User <ec2-user@ip-172-31-45-39.ec2.internal>
2021-02-07 09:14:13 +0000

    課題4

---
+++ app/models/favorite.rb
@@ -0,0 +1,7 @@
+class Favorite < ApplicationRecord
+  belongs_to :user
+  belongs_to :book
+
+  validates :user, uniqueness: { scope: :book_id }
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get 'book_comments/create'
+  get 'book_comments/destroy'
   get 'users/show'
   get 'home/about'
   root to: 'home#top'
   devise_for :users
-  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update]
+  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update] do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
   resources :users, only: [:show, :edit, :update, :index]
 end
